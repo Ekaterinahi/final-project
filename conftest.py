4dab67235649d0ae9b53d1308c0c61e15f3b111c
unknown <katena-kozina@mail.ru>
2022-07-05 00:02:45 +0300

    gene

--- conftest.py
+++ conftest.py
@@ -1,22 +1,24 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from .pages.product_page import ProductPage
 
-def pytest_addoption(parser): #парсим язык
-    parser.addoption('--language', action='store', default='en',
-                    help="Choose language")
-
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',)
+    parser.addoption('--language', action="store", default='en', help="Choose from langs: (en/ru/es/...)")
 
 @pytest.fixture(scope="function")
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    if (browser_name == "chrome"):
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
 
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(options=options)
-
+    else:
+        print("Browser<browser_name> still is not implemented")
     yield browser
-    print("\nquit browser..")
+    print("\nQuit browser...")
     browser.quit()
