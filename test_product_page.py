b282af21a9ec0bb3d4278c8b9edf3816395fc5ff
unknown <katena-kozina@mail.ru>
2022-07-05 15:53:51 +0300

    open basket from product page test

--- test_product_page.py
+++ test_product_page.py
@@ -9,10 +9,10 @@ from .pages.product_page import ProductPage
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(autouse=True)
     def setup(self, browser):
-        linka = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-        email = str(time.time()) + "@fakemail.org"
-        password = "426244123ABCdefhg"
-        product_page = ProductPage(browser, linka)
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        email = str(time.time()) + "@katemail.org"
+        password = "426244123ABhdic"
+        product_page = ProductPage(browser, link)
         product_page.open()
         product_page.go_to_login_page()
         page = LoginPage(browser, browser.current_url)
@@ -22,8 +22,8 @@ class TestUserAddToBasketFromProductPage():
 
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
-        linka = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-        page = ProductPage(browser, linka)
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
         page.open()
         page.add_to_cart()
         page.solve_quiz_and_get_code()
@@ -31,8 +31,8 @@ class TestUserAddToBasketFromProductPage():
         page.should_be_correct_adding_product_price()
 
     def test_user_cant_see_success_message(self, browser):
-        linka = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-        page = ProductPage(browser, linka)
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
@@ -49,7 +49,6 @@ class TestUserAddToBasketFromProductPage():
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
-
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
@@ -58,23 +57,23 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_correct_adding_product_price()
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    linkk = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, linkk)
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
     page.should_not_be_success_message()
 
 def test_guest_cant_see_success_message(browser):
-    linkk = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, linkk)
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    linkk = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, linkk)
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
@@ -89,7 +88,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser,link)
+    page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
