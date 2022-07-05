4faaf6b46e4fc141f731aa20bc2c7e56cf7ac29b
unknown <katena-kozina@mail.ru>
2022-07-05 15:51:44 +0300

    page object basket page

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,7 +1,6 @@
 from .base_page import BasePage
 from .locators import BasketPageLocators
 
-
 class BasketPage(BasePage):
     def basket_is_empty(self):
         assert self.is_not_element_present(*BasketPageLocators.SOME_ITEM), \
